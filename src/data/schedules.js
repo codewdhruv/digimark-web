e80c3d0185e427e26eb9a41192ad4e6bb2c2fedd
dhrubajyoti89 <dhruba.chakraborty2019@gift.edu.in>
2022-07-20 10:30:22 +0530

    devops demo

--- src/data/schedules.js
+++ src/data/schedules.js
@@ -2,11 +2,11 @@ export const ScheduleData = [
 	{
 		label: "Day 1",
 		topic: "Mastering SEO, Content Marketing, PPC and Digital Analytics",
-		date: "Monday, 20th June 2022",
+		date: "Saturday, 25th June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/1.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -58,11 +58,11 @@ export const ScheduleData = [
 	{
 		label: "Day 2",
 		topic: "Expert Opinion & Feedback by @Team LikeMinds",
-		date: "Tuesday, 21st June 2022",
+		date: "Sunday, 26th June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/2.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -114,11 +114,11 @@ export const ScheduleData = [
 	{
 		label: "Day 3",
 		topic: "Mastering Social Media, Mobile Marketing and Digital Strategy",
-		date: "Wednesday, 22nd June 2022",
+		date: "Monday, 27th June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/3.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -170,11 +170,11 @@ export const ScheduleData = [
 	{
 		label: "Day 4",
 		topic: "Expert Opinion & Feedback by @Team Commudle",
-		date: "Thursday, 23rd June 2022",
+		date: "Tuesday, 28th June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/4.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -226,11 +226,11 @@ export const ScheduleData = [
 	{
 		label: "Day 5",
 		topic: "Advanced Web Analytics",
-		date: "Friday, 24th June 2022",
+		date: "Wednesday, 29th June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/5.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -282,11 +282,11 @@ export const ScheduleData = [
 	{
 		label: "Day 6",
 		topic: "Expert Opinion & Feedback by @Team Languify",
-		date: "Saturday, 25th June 2022",
+		date: "Thursday, 3oth June 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/6.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -338,11 +338,11 @@ export const ScheduleData = [
 	{
 		label: "Day 7",
 		topic: "Advanced Search Engine Optimization (SEO)",
-		date: "Sunday, 26th June 2022",
+		date: "Friday, 1st July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/7.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -394,11 +394,11 @@ export const ScheduleData = [
 	{
 		label: "Day 8",
 		topic: "Expert Opinion & Feedback by @Team Be Yogya",
-		date: "Monday, 27th June 2022",
+		date: "Saturday, 2nd July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/8.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -450,11 +450,11 @@ export const ScheduleData = [
 	{
 		label: "Day 9",
 		topic: "Advanced Social Media",
-		date: "Tuesday, 28th June 2022",
+		date: "Sunday, 3rd July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/9.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -506,11 +506,11 @@ export const ScheduleData = [
 	{
 		label: "Day 10",
 		topic: "Expert Opinion & Feedback by @Team ZSPUN",
-		date: "Wednesday, 29th June 2022",
+		date: "Monday, 4th July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/10.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -562,11 +562,11 @@ export const ScheduleData = [
 	{
 		label: "Day 11",
 		topic: "Final Capstone Digital Marketing Project Session 1",
-		date: "Friday, 1st July 2022",
+		date: "Tuesday, 5th July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/11.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -618,11 +618,11 @@ export const ScheduleData = [
 	{
 		label: "Day 12",
 		topic: "Final Capstone Digital Marketing Project Session 2",
-		date: "Saturday, 2nd July 2022",
+		date: "Wednesday, 6th July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/12.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -674,11 +674,11 @@ export const ScheduleData = [
 	{
 		label: "Day 13",
 		topic: "Placement Assistance Session 1",
-		date: "Sunday, 3rd July 2022",
+		date: "Thursday, 7th July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/13.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
@@ -730,11 +730,11 @@ export const ScheduleData = [
 	{
 		label: "Day 14",
 		topic: "Placement Assistance Session 2",
-		date: "Monday, 4th July 2022",
+		date: "Friday, 8th July 2022",
 		time: "18:15 - 21.00 IST",
 		thumbnail: require("../assets/images/14.png").default,
 		celendar_url:
-			"#",
+			"https://calendar.google.com/event?action=TEMPLATE&tmeid=NzVlc2g2YmppaWZqcGd2Y3Vxamozb2oxbmJfMjAyMjA2MjVUMTgzMDAwWiB0ZWFtQGxlYXJuZm9yY2F1c2UuY29t&tmsrc=team%40learnforcause.com&scp=ALL",
 		youtube_url: "#",
 		agenda: [
 			{
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
 					<div className="space-y-3">
 						<h1 className="text-4xl text-blue-700 font-semibold">Speakers</h1>
 						<p className="text-lg text-gray-500">
-							Our speakers are influential folks & allies who have been
+							Our instructors are influential folks & allies who have been
 							associated with communities within their organisations, cities,
 							country and beyond.
 						</p>
